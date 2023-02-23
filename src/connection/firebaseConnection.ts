44d2bf39886fa1cbe163ed80f736cbde68ad49f6
kallif <kallifabrahao@gmail.com>
2023-02-23 01:41:35 -0300

    add: :hammer: adding env example

--- src/connection/firebaseConnection.ts
+++ src/connection/firebaseConnection.ts
@@ -3,12 +3,12 @@ import "firebase/database"
 import "firebase/auth"
 
 const firebaseConfig = {
-	apiKey: "AIzaSyBI6KIDn_AvgUJg0kQzjkA76BtDUk8_H8A",
-	authDomain: "slicassianoricardo-78ae9.firebaseapp.com",
-	projectId: "slicassianoricardo-78ae9",
-	storageBucket: "slicassianoricardo-78ae9.appspot.com",
-	messagingSenderId: "1075094855720",
-	appId: "1:1075094855720:web:d26983b5f89a182775551d",
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+	appId: process.env.APP_ID,
 }
 
 // Initialize Firebase
